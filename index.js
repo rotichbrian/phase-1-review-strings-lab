53f9495f9613b40c031c1908ce432c18cd9331e4
Brian Kipkorir <rotichbrian891@gmail.com>
2024-03-18 12:16:12 +0300

    completed string-review

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+let currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+currentUser = 'Grace Hopper';
+const WelcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = WelcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
